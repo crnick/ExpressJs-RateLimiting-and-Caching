1627fa7f153599a21570b6e4019bea1eb28feeb2
nikhil <nick.nj94@gmail.com>
2023-08-05 17:25:33 +0530

    server with rate limit and cache

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+//rate limit 100 within 10 mins
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, //10 mins
+  max: 5, // how many request
+});
+
+//in http header x-ratelimit-limit / x-ratelimit-remaining
+app.use(limiter);
+app.set("trust proxy", 1); // using it as proxy
+
+app.use("/api", require("./routes/weather"));
+
+app.use(cors());
+
+app.listen(PORT, () => console.log("server started"));
